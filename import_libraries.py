b13e71d0bc0c2c9c9a874e9607ac13f916aad0ac
Sienna Coward <scoward@unb.ca>
2021-10-31 15:16:14 -0300

    matplotlib imported, example plot included

--- import_libraries.py
+++ import_libraries.py
@@ -1,4 +1,8 @@
-# this code will be used to import libraries
+# importing matplotlib 
+import matplotlib.pyplot as plt
+import numpy as np
 
-msg = "Hello World"
-print(msg)
+x = np.linspace(0, 20, 100)     # create a list of evenly space numbers over the range
+range
+plt.plot(x, np.sin(x))          # plot the sine of each x point
+plt.show()
