b0c5f7432265f5f2fac58a7c7967355a4405932b
Sienna Coward <scoward@unb.ca>
2021-10-31 16:16:17 -0300

    instructions for importing libraries

--- import_libraries.py
+++ import_libraries.py
@@ -1,8 +1,13 @@
-# importing matplotlib 
+# importing libraries
+
+import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
+
 
 x = np.linspace(0, 20, 100)     # create a list of evenly space numbers over the range
 range
 plt.plot(x, np.sin(x))          # plot the sine of each x point
 plt.show()
+
