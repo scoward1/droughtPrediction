bb96e64ca92579a8e44eb90f8251f2b1431e2144
Max Hartling <mhartlin@Unb.ca>
2021-11-02 10:32:54 -0300

    test and validation data imported

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 # importing libraries
 
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 # import seaborn as sns
 import numpy as np
 import os
 
 print(os.getcwd())
-os.chdir('/Users/sienn/Documents/uni/engg4553/project/droughtPrediction')
+#os.chdir('/Users/sienn/Documents/uni/engg4553/project/droughtPrediction')
+os.chdir('/Users/Acer/Desktop/4th Year/Python/Import Data')
 print(os.getcwd())
 
-train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
-train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
+#train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
+#train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
 
-print(train1.head())
-print(train2['fips'])
+test = pd.read_csv('Washington_test.txt', sep = ",", header = 0)
+
+validation = pd.read_csv('Washington_validation.txt', sep = ",", header = 0)
+
+#print(train1.head())
+#print(train2['fips'])
+
+print(test.head())
+
+print(validation.head())
