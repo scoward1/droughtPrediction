5ed43f71078e55d2eeea17fe5c29a04fbe4f9de2
Sienna Coward <scoward@unb.ca>
2021-11-09 11:35:55 -0400

    fixed headers for datasets, fixed validation set

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ import numpy as np
 import os
 
 print(os.getcwd())
-#os.chdir('/Users/sienn/Documents/uni/engg4553/project/droughtPrediction')
-os.chdir('/Users/Acer/Desktop/4th Year/Python/Import Data')
+os.chdir('E:/uni/year4/ece4553/project/localRepo/droughtPrediction')
+#os.chdir('/Users/Acer/Desktop/4th Year/Python/Import Data')
 print(os.getcwd())
 
-#train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
-#train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
-
+# import the data
+train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
+train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
 test = pd.read_csv('Washington_test.txt', sep = ",", header = 0)
-
 validation = pd.read_csv('Washington_validation.txt', sep = ",", header = 0)
 
-#print(train1.head())
+print(train1.head())
 #print(train2['fips'])
 
 print(test.head())
