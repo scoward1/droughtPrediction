7b426c0eb4cdc4164f9d9c2cfb7410919f2bd71e
Sienna Coward <scoward@unb.ca>
2021-11-09 23:04:14 -0400

    created interpolated array for dlevels, used mrmr to get the top 10 features

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 # importing libraries
-
 import pandas as pd
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # import seaborn as sns
 import numpy as np
+from scipy.interpolate import interp1d
+from mrmr import mrmr_classif
 import os
+import math
+
+def interpolate_nans(padata, pkind='linear'):
+    aindexes = np.arange(padata.shape[0])
+    agood_indexes, = np.where(np.isfinite(padata))
+    f = interp1d(agood_indexes
+               , padata[agood_indexes]
+               , bounds_error=False
+               , copy=False
+               , fill_value="extrapolate"
+               , kind=pkind)
+    return f(aindexes)
 
 print(os.getcwd())
-os.chdir('E:/uni/year4/ece4553/project/localRepo/droughtPrediction')
-#os.chdir('/Users/Acer/Desktop/4th Year/Python/Import Data')
+os.chdir('D:/uni/year4/ece4553/project/localRepo/droughtPrediction')        # locRepo for sie
+#os.chdir('/Users/Acer/Desktop/4th Year/Python/Import Data')                # locRepo for max
 print(os.getcwd())
 
-# import the data
+# read the data. train set brought in in two parts because it was too large to
+# do at the same time
 train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
 train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
 test = pd.read_csv('Washington_test.txt', sep = ",", header = 0)
 validation = pd.read_csv('Washington_validation.txt', sep = ",", header = 0)
 
-print(train1.head())
-#print(train2['fips'])
+# rearrange the information to work with the MRMR feature selection
+frames = [train1, train2]
+train = pd.concat(frames)                       # putting both train sets dataframes together
+dlevel = train['score']                         # creating a class series
+dlevel = dlevel.values                          # changing from series to numpy array
+train = train.drop(columns = "score")           # dropping the class from the dataframe
 
-print(test.head())
+inter_dlevel = interpolate_nans(dlevel)
 
-print(validation.head())
+selected_features = mrmr_classif(train, inter_dlevel, 10)
+print(selected_features)
