34cae4e1eecb04fecb7b9df84b57fc0622d663ac
Sienna Coward <scoward@unb.ca>
2021-11-10 00:16:08 -0400

    normalized the data

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pandas as pd
 # import seaborn as sns
 import numpy as np
 from scipy.interpolate import interp1d
+from sklearn.preprocessing import StandardScaler
+# from sklearn.decomposition import PCA
 from mrmr import mrmr_classif
 import os
 import math
@@ -29,21 +31,29 @@ os.chdir('D:/uni/year4/ece4553/project/localRepo/droughtPrediction')        # lo
 print(os.getcwd())
 
 # read the data. train set brought in in two parts because it was too large to
-# do at the same time
+# do at the same time. turned into numpy arrays to allow for normalization of data
 train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
 train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
+frames = [train1, train2]
+train = pd.concat(frames)                       # putting both train sets dataframes together
 test = pd.read_csv('Washington_test.txt', sep = ",", header = 0)
 validation = pd.read_csv('Washington_validation.txt', sep = ",", header = 0)
 
 # rearrange the information to work with the MRMR feature selection
-frames = [train1, train2]
-train = pd.concat(frames)                       # putting both train sets dataframes together
 dlevel = train['score']                         # creating a class series
 dlevel = dlevel.values                          # changing from series to numpy array
 train = train.drop(columns = "score")           # dropping the class from the dataframe
 
+# normalize the data using the sklearn package (keep the column and index names by using .iloc)
+scaler = StandardScaler()
+train.iloc[:, 3:-1] = scaler.fit_transform(train.iloc[:,3:-1].to_numpy())
+validation.iloc[:, 3:-1] = scaler.fit_transform(validation.iloc[:,3:-1].to_numpy())
+test.iloc[:, 3:-1] = scaler.fit_transform(test.iloc[:,3:-1].to_numpy())
+
+# linearly interpolate the missing dlevels
 inter_dlevel = linInter_NaN(dlevel)
 
+# use mrmr to select the 10 best features
 selected_features = mrmr_classif(train, inter_dlevel, 10)
 """
 see: pip install git+https://github.com/smazzanti/mrmr
