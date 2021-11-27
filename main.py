86c57bf74158793b9526ea31f836dad35b07fa3a
Sienna Coward <scoward@unb.ca>
2021-11-27 00:58:12 -0400

    fixed the Washington_test.txt score column

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # import seaborn as sns
 import numpy as np
-from numpy import mean, std
+from numpy import inner, mean, std
 from scipy.interpolate import interp1d
 from sklearn.preprocessing import StandardScaler
 import os
-import math
 from featureReduction import mrmr_fun, sfs_fun
 from dimensionReduction import ica_fun, pca_fun
-import sys
 from dimensionReduction import pca_fun
 from models import qda_fun, knn_fun, knn_neighbors, linReg_fun
 
@@ -30,112 +28,90 @@ def linInter_NaN(nanData, pkind='linear'):
                , kind=pkind)
     return f(aindexes)
 
-#os.chdir('D:/uni/year4/ece4553/project/localRepo/droughtPrediction')        # locRepo for sie
-os.chdir('/Users/Acer/Documents/Drought')                                  # locRepo for max
+os.chdir('D:/uni/year4/ece4553/project/localRepo/droughtPrediction')        # locRepo for sie
+# os.chdir('/Users/Acer/Documents/Drought')                                  # locRepo for max
 
 # read the data. train set brought in in two parts because it was too large to
 # do at the same time. turned into numpy arrays to allow for normalization of data
 train1 = pd.read_csv('washington_trainseries.txt', sep = ",", header = 0)
 train2 = pd.read_csv('washington_trainseries2.txt', sep = ",", header = 0)
-train = pd.concat([train1, train2], ignore_index=True)                       # putting both train sets dataframes together
 test = pd.read_csv('Washington_test.txt', sep = ",", header = 0)
 validation = pd.read_csv('Washington_validation.txt', sep = ",", header = 0)
-train = pd.concat([train, validation, test], ignore_index= True)
+frames = [train1, train2, validation, test]
+train = pd.concat(frames, ignore_index= True)
 
 # rearrange the information to work with the MRMR feature selection
-dlevel = train['score']                                                # creating a class series
-dlevel = dlevel.values                                           # changing from series to numpy array
-train = train.drop(columns = "score")                                        # dropping the class from the dataframe
+dlevel = pd.Series(train["score"].values)                               # creating a class series
+train = train.drop(columns = "score")                                   # dropping the class from the dataframe
 
-# normalize the data using the sklearn package (keep the column and index names by using .iloc)
-scaler = StandardScaler()
-train.iloc[:, 2:20] = scaler.fit_transform(train.iloc[:, 2:20].to_numpy())
-validation.iloc[:, 2:20] = scaler.fit_transform(validation.iloc[:, 2:20].to_numpy())
-test.iloc[:, 2:20] = scaler.fit_transform(test.iloc[:, 2:20].to_numpy())
+# change location variable
+Location = train.iloc[:,0]
 
-#Change Location column
+count = 1
 
-Location = train.iloc[0:242190,0]
-
-count=1
-
-for x in range (242190):
+for x in range (len(Location)):
     if(x<= 6210*count):
         Location[x] = count
         if (x % 6210 == 0 and x!=0):
               count = count+1
 
-#Break Apart Date
+# break apart date
+new_date = train
+new_date.iloc[:,1] = pd.to_datetime(new_date.iloc[:,1], format = '%Y-%m-%dT', errors = 'coerce')
 
-new_date=train
-print(new_date.date.head())
-
-new_date.iloc[0:242190,1] = pd.to_datetime(new_date.iloc[0:242190,1], 
-format = '%Y-%m-%dT',
-errors = 'coerce')
-
-#Date Split in to year-month-day
-new_date['year'] = new_date.iloc[0:242190,1].dt.year
-new_date['month'] =  new_date.iloc[0:242190,1].dt.month
-new_date['day'] = new_date.iloc[0:242190,1].dt.day
+# split date into year-month-day
+new_date['year'] = new_date.iloc[:,1].dt.year
+new_date['month'] =  new_date.iloc[:,1].dt.month
+new_date['day'] = new_date.iloc[:,1].dt.day
 
 new_date.groupby('year').size()
 new_date.groupby('month').size()
 new_date.groupby('day').size()
 
-
-print(new_date[['year','month','day']].head())
+# drop orginal date and location columns
 train = train.drop(columns = "date")   
-train = train.drop(columns = "fips")   
+train = train.drop(columns = "fips")
 
-#Add Split date to training set
+# add new date data to training set
 train['day'] = new_date.day
 train['month'] = new_date.month
 train['year'] = new_date.year
-
 train['Location'] = Location
-print(train.head())
+# print(train.head())
 
-# changing date from object to int, train data from float64 to int64
-#train['date'] = (pd.to_datetime(train['date']).dt.strftime("%Y%m%d")).astype(np.int64)
-# train['date'] = (pd.to_datetime(train['date']).dt.strftime("%Y%m%d")).astype(np.int64)
+# normalize the data using the sklearn package (keep the column and index names by using .iloc)
+scaler = StandardScaler()
+train.iloc[:, 0:22] = scaler.fit_transform(train.iloc[:, 0:22].to_numpy())
 
 # linearly interpolate the missing dlevels
 inter_dlevel = linInter_NaN(dlevel)
 inter_dlevel_int = inter_dlevel.astype(np.int64)
 
 # built-in pymrmr requries a df where the first col is the class, the rest are features
-# take out the date and location because the number mess it up
 inter_dlevel_df = pd.DataFrame(inter_dlevel_int)
 inter_dlevel_df.columns = ['score']
-#mrmr_df = inter_dlevel_df.join(train)
-
-# determine best 10 features using 3 different types of mrmr
-#top_features = mrmr_fun(mrmr_df, train, inter_dlevel)
-mrmr_df = inter_dlevel_df.join(train.iloc[:, 4:20])
+mrmr_df = inter_dlevel_df.join(train)
 
 # determine best 10 features using mrmr
-top_features = mrmr_fun(mrmr_df, train, inter_dlevel_int)
+top_features = mrmr_fun(mrmr_df, train, inter_dlevel)
+train_fr = train.filter(top_features, axis = 1)
 
 # use SFS to select 10 best features, use 50,000 points to run
 #train_sfs = train.iloc[1:50000,3:20]
 #dlevel_sfs = inter_dlevel_int[1:50000]
 #sfs_fun(train_sfs, dlevel_sfs)
 
-# only use the top 10 features of the train dataframe
-train_fr = train.filter(top_features, axis = 1)
-
 # use PCA, return new lower dimension training data
 train_dr = pca_fun(train_fr)
 
 # QDA
-# qda_acc = qda_fun(train_dr, train_inter_dlevel_int)
-# print('Accuracy: %.3f (%.3f)' % (mean(qda_acc), std(qda_acc)))
+qda_acc = qda_fun(train_dr, inter_dlevel_int)
+print('Accuracy: %.3f (%.3f)' % (mean(qda_acc), std(qda_acc)))
 
 # KNN - optimal k-value determined by knn_neighbors (only have to use once)
-# knn_neighbors(train_dr, train_inter_dlevel_int)
+# knn_neighbors(train_dr, inter_dlevel_int)
 # knn_neighbors = 100
-# knn_acc = knn_fun(train_dr, train_inter_dlevel_int, knn_neighbors)
+# knn_acc = knn_fun(train_dr, inter_dlevel_int, knn_neighbors)
 # print(knn_acc)
 
 # linear regression
