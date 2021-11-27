2ffb4282f28cd2d9e951b44afcb8ee29268030b8
Max Hartling <mhartlin@Unb.ca>
2021-11-26 21:11:12 -0400

    nothing changed

--- dimensionReduction.py
+++ dimensionReduction.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.decomposition import FastICA
 
 def pca_fun(train):
     # all feature reduction was done to get 10 features
@@ -21,3 +22,11 @@ def pca_fun(train):
     reduced_train = reduced_train[:, 0:num_feat]
 
     return reduced_train
+
+
+def ica_fun(train):
+        ICA = FastICA(n_components=10)
+        reduced_train_ICA = FastICA.fit_transform(train)
+
+        return reduced_train_ICA
+
